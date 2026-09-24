fd7d9a70b78a7edb037703128bb8ae3022a36013
Mohammad Sabihul Haque <sabihul2024@gmail.com>
2026-09-24 15:54:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.7 MB (36.01%)

--- 0009-palindrome-number/solution.cpp
+++ 0009-palindrome-number/solution.cpp
@@ -2,11 +2,15 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if (x < 0) return false;
-        
-        string original = to_string(x);
-        string reversedStr = original;
-        reverse(reversedStr.begin(), reversedStr.end());
-        return original == reversedStr;
+
+        long long reversedNum = 0;
+        int temp = x;
+        while (temp > 0) {
+            int lastDigit = temp % 10;
+            reversedNum = (reversedNum * 10) + lastDigit;
+            temp = temp / 10;
+        }
+        return x == reversedNum;
     }
 };
 
